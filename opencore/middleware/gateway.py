d9575aaa332ac1070a7f0e0f524751d524bbc5a7
lonetwin <steve@lonetwin.net>
2012-02-22 20:34:52 +0530

    Misc enhancements
    * optional parameter to specify a regular expression to match the request
      path for which the gateway should be skipped. Defaults to
      '\.(js|css|png|gif|jpg|jpeg|ico)/?$'
    * optional parameter to specify a list of headers along with regular
      expressions to match the corresponding values. If a match is found, the
      gateway would be skipped.
    * make `domain` an optional parameter. Defaults to request.host if not
      specified. (This reduces config changes during deployment across multiple
      domains).

--- opencore/middleware/gateway.py
+++ opencore/middleware/gateway.py
@@ -16,35 +16,72 @@
 # 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 import os
+import re
 from webob import Request, Response
 from chameleon.zpt.loader import TemplateLoader
 
 DEFAULT_COOKIE_LIFETIME = 10 * 365 * 24 * 60 * 60 # 10 years
 
 class GatewayMiddleware(object):
-    def __init__(self, app, global_conf, password, cookie_name, cookie_value, domain, lifetime=DEFAULT_COOKIE_LIFETIME, default_message=''):
+    def __init__(self,
+                 app,
+                 global_conf,
+                 password,
+                 cookie_name,
+                 cookie_value,
+                 domain = '',
+                 lifetime = DEFAULT_COOKIE_LIFETIME,
+                 # - exclude these URLs from gateway
+                 exclude = '\.(js|css|png|gif|jpg|jpeg|ico)/?$',
+                 # - exclude requests with these headers from gateway
+                 skip_with_header = ''
+                 ):
+        """ example paste.deploy config:
+
+            [filter:gateway]
+            use = egg:openideo#gateway
+            password = T0pS3cRet
+            cookie_name = oreo
+            cookie_value = yummy
+            exclude = /static
+            skip_with_header = HTTP_USER_AGENT:(funkload|ApacheBench)
+                               REMOTE_HOST:127.0.0.1
+        """
         self.app = app
         self.password = password
         self.cookie_name = cookie_name
         self.cookie_value = cookie_value
         self.domain = domain
         self.lifetime = lifetime
-        self.default_message = default_message
+        self.exclude = re.compile(exclude, re.I) if exclude else None
+        self.skip_list = []
+        if skip_with_header:
+            hdr_pattern_list = [ kv.split(':') for kv in skip_with_header.split() ]
+            self.skip_list = [ (header, re.compile(pattern, re.I)) for header, pattern in hdr_pattern_list ]
         self.loader = TemplateLoader(os.path.dirname(__file__))
-    
+
     def __call__(self, environ, start_response):
         request = Request(environ)
-        password = request.params.get('password', None)
 
         if request.cookies.get(self.cookie_name, None) == self.cookie_value:
             return self.app(environ, start_response)
 
-        elif password == self.password:
-            res = request.get_response(self.app)
-            res.set_cookie(self.cookie_name, self.cookie_value, max_age=self.lifetime, path='/',
-                           domain=self.domain)
-            return res(environ, start_response)
+        if self.exclude and re.search(self.exclude, request.path_info):
+            return self.app(environ, start_response)
 
+        for header, pattern in self.skip_list:
+            value = environ.get(header)
+            if value and re.search(pattern, value):
+                return self.app(environ, start_response)
+
+        password = request.params.get('password', None)
+
+        if password == self.password:
+            res = request.get_response(self.app)
+            domain = self.domain if self.domain else request.host
+            res.set_cookie(self.cookie_name, self.cookie_value,
+                            max_age=self.lifetime, path='/', domain=domain)
+            return res(environ, start_response)
         else:
             template = self.loader.load('form.pt')
             res = Response(content_type='text/html', charset='utf8', body=template.render())
